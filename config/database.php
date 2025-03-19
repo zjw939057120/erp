ff81d51680d1a6ba63a232cfbdba50f0601930a8
zjw939057120 <zjw939057120@outlookcom>
2025-03-19 20:23:02 +0800

    安装

--- config/database.php
+++ config/database.php
@@ -18,15 +18,15 @@ return [
             // 服务器地址
             'hostname'           =>  '127.0.0.1',
             // 数据库名
-            'database'           =>  'oa',
+            'database'           =>  'erp',
             // 用户名
             'username'           =>  'root',
             // 密码
-            'password'           =>  '123456',
+            'password'           =>  'password',
             // 端口
             'hostport'           =>  '3306',
             // 数据库表前缀
-            'prefix'             =>  'oa_',
+            'prefix'             =>  'erp_',
             // 数据库连接参数
             'params'          => [],
             // 数据库编码默认采用utf8mb4
